c71c2c00a7f1bdc635e942c76d1628006f8ee883
Allan Degnan <allan@adegnan.net>
2015-09-06 19:38:01 +0100

    RC2

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
 # cron doesn't have acces to env, this is simpler than fixing that.
-useradd -D ${CRON_USER}
+useradd  ${CRON_USER}
 
-# Sleep the interval.
-sleep ${CRON_TIME}
+while true; do
+  # Sleep the interval.
+  sleep ${CRON_TIME}
 
-# Run the CRON COMMAND
-${CRON_CMD}
-
-# Should happily exit, then the container will restart and sleep again.
+  # Run the CRON COMMAND
+  ${CRON_CMD}
+done;
